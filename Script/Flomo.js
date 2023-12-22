638a138a2fcea394430eb58aca9827c5f21c33ae
Ziyue Nie <32543441+alsoeoe@users.noreply.github.com>
2023-12-22 18:03:47 +0800

    更新

--- Script/Flomo.js
+++ Script/Flomo.js
@@ -1,6 +1,6 @@
 const modifiedResponse = {};
 const responseBody = JSON.parse($response ? $response.body : null);
-responseBody.data.pro_expired_a = "2088-08-08 08:08:08";
+responseBody.data.pro_expired_at = "2088-08-08 08:08:08";
 responseBody.data.referee_pro_days = "8888";
 modifiedResponse.body = JSON.stringify(responseBody);
 $done(modifiedResponse);
