d54ff3df035434ac9c3210edb9307f299d89d01a
also <nieziyue@hotmail.com>
2024-01-02 09:29:04 +0800

    更新

--- Script/BuyiTunes.js
+++ Script/BuyiTunes.js
@@ -23,6 +23,11 @@ const productIDMap = {
     product_id: "com.kobaltlab.HashPhotos.iap.pro1year",
     mark: 0,
   },
+  "com.stress.test.record": {
+    name: "解压小橙子",
+    product_id: "com.stress.test.record.yearly",
+    mark: 0,
+  },
 };
 
 // 固定部分
