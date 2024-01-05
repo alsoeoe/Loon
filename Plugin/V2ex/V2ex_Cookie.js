0c69f888a40a85756f86f9f729f14f3f58c66fbb
also <nieziyue@hotmail.com>
2024-01-05 18:02:06 +0800

    更新

--- Plugin/V2ex/V2ex_Cookie.js
+++ Plugin/V2ex/V2ex_Cookie.js
@@ -1,22 +1,27 @@
 const cookieName = "V2EX";
-const cookieKey = "V2ex_Cookie";
+const cookieKey = "chavy_cookie_v2ex";
 const cookieVal = $request.headers["Cookie"] || $request.headers["cookie"];
 
 if (cookieVal) {
   const existingCookieVal = $persistentStore.read(cookieKey);
-  if (cookieVal !== existingCookieVal) {
-    // 如果新的cookie值与存储中的值不同
+  // 检查是否已有cookie值
+  if (!existingCookieVal) {
+    // 如果没有现有的cookie值，则写入并发送通知
     let cookie = $persistentStore.write(cookieVal, cookieKey);
     if (cookie) {
-      let msg = `${cookieName}`;
-      $notification.post(msg, "Cookie写入成功", "详见日志");
-      console.log(msg);
+      let msg = "Cookie写入成功";
+      console.log(`${cookieName}: ${msg}`);
       console.log("新的Cookie值: " + cookieVal);
+      $notification.post(cookieName, msg, "详见日志");
     }
+  } else if (cookieVal !== existingCookieVal) {
+    // 如果新的cookie值与存储中的值不同，更新但不发送通知
+    $persistentStore.write(cookieVal, cookieKey);
+    console.log(`${cookieName}: Cookie更新成功`);
+    console.log("更新后的Cookie值: " + cookieVal);
   } else {
-    // 如果新的cookie值与存储中的值相同，则只记录在日志中，不发送通知
-    let msg = `${cookieName}: 重复的Cookie值，不更新`;
-    console.log(msg);
+    // 如果新的cookie值与存储中的值相同，只记录在日志中，不发送通知
+    console.log(`${cookieName}: 重复的Cookie值，不更新`);
     console.log("重复的Cookie值: " + cookieVal);
   }
 }
