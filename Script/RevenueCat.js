77dbcbcc34866d1491462d703dd4e141c0f9fd12
also <nieziyue@hotmail.com>
2024-02-27 10:33:04 +0800

    更新

--- Script/RevenueCat.js
+++ Script/RevenueCat.js
@@ -3,14 +3,26 @@ const responseBody = JSON.parse(
   (typeof $response != "undefined" && $response.body) || null
 );
 
-if (typeof $response == "undefined") {
-  // 处理请求
-  delete $request.headers["x-revenuecat-etag"];
-  delete $request.headers["X-RevenueCat-ETag"];
-  modifiedResponse.headers = $request.headers;
-} else if (responseBody && responseBody.subscriber) {
-  // 处理响应
-  const UA = $request.headers["user-agent"];
+function normalizeHeaders(headers) {
+  console.log(
+    `在我们开始之前，让我们看看这些神秘的请求头部：${JSON.stringify(headers)}`
+  ); // 打印原始请求头
+  if (headers["User-Agent"]) {
+    headers["user-agent"] = headers["User-Agent"];
+    delete headers["User-Agent"];
+  }
+  delete headers["x-revenuecat-etag"];
+  delete headers["X-RevenueCat-ETag"];
+  console.log(
+    `经过一番魔法处理，请求头现在干净多了: ${JSON.stringify(headers)}`
+  ); // 打印正规化后的请求头
+  return headers;
+}
+
+function modifyResponseBody(body) {
+  console.log(`看看我们拿到的宝贝是什么: ${JSON.stringify(body)}`); // 打印原始响应体
+
+  // 定义UA映射和订阅数据
   const UAMappings = {
     // 谜底时钟
     // https://apps.apple.com/cn/app/id1536358464
@@ -106,6 +118,7 @@ if (typeof $response == "undefined") {
   };
 
   const subscriptionData = {
+    // 订阅数据
     is_sandbox: !1,
     ownership_type: "PURCHASED",
     billing_issues_detected_at: null,
@@ -119,35 +132,40 @@ if (typeof $response == "undefined") {
   };
 
   const entitlementData = {
+    // 权益数据
     grace_period_expires_date: null,
     purchase_date: "2022-09-09T09:09:09Z",
     expires_date: "2099-09-09T09:09:09Z",
   };
 
-  for (const appName in UAMappings) {
-    if (new RegExp(`^${appName}`, "i").test(UA)) {
-      console.log(`找到匹配：${appName}\n`);
-      const { name, id } = UAMappings[appName];
-      responseBody.subscriber = {
-        non_subscriptions: {},
-        first_seen: "2022-09-09T09:09:08Z",
-        original_application_version: "8",
-        other_purchases: {},
-        management_url: "https://apps.apple.com/account/subscriptions",
-        subscriptions: {},
-        entitlements: {},
-        original_purchase_date: "2022-09-08T08:08:08Z",
-        original_app_user_id: "$RCAnonymousID:also",
-        last_seen: "2022-09-09T01:04:03Z",
-      };
-      responseBody.subscriber.subscriptions = { [id]: subscriptionData };
-      responseBody.subscriber.entitlements = {
-        [name]: { ...entitlementData, product_identifier: id },
-      };
-      break;
-    }
+  if (body && body.subscriber) {
+    const UA = $request.headers["user-agent"] || $request.headers["User-Agent"];
+    console.log(`嗯，这个User-Agent闻起来像... ${UA}`); // 打印检测到的User-Agent
+    Object.keys(UAMappings).forEach((appName) => {
+      if (new RegExp(`^${appName}`, "i").test(UA)) {
+        console.log(`哇塞，我们钓到一只大鱼: ${appName}`); // 打印匹配到的应用
+        const { name, id } = UAMappings[appName];
+        body.subscriber.subscriptions[id] = subscriptionData;
+        body.subscriber.entitlements[name] = {
+          ...entitlementData,
+          product_identifier: id,
+        };
+      }
+    });
   }
-  modifiedResponse.body = JSON.stringify(responseBody);
+
+  console.log(`魔法施展后，宝贝现在变成了: ${JSON.stringify(body)}`); // 打印修改后的响应体
+  return body;
+}
+
+if (responseBody && responseBody.subscriber) {
+  console.log("精灵和魔法师们，开始我们的表演吧！");
+  $request.headers = normalizeHeaders($request.headers);
+  const UA = $request.headers["user-agent"];
+  modifiedResponse.body = JSON.stringify(modifyResponseBody(responseBody));
+} else {
+  console.log("精灵和魔法师们，开始我们的表演吧！");
+  modifiedResponse.headers = normalizeHeaders($request.headers);
 }
 
 $done(modifiedResponse);
